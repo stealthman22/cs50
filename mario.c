fa0f970c42e460700e0c6e9b8c7c1e87e388d6b1
stealthman22 <tdotonline@gmail.com>
2023-01-08 23:05:56 +0000

    adding meow and mario files

---
+++ mario.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+ // printf("????\n");
+ for (int i =0; i < 4; i++)
+ {
+  printf("?");
+ }
+ printf("\n");
+}
---
+++ meow.c
@@ -0,0 +1,22 @@
+// To use true and false as keywords, you have to call in this header
+// #include <stbool.h>
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+ // While loop
+// int i = 0;
+// while(i < 30)
+// {
+//   printf("meow\n");
+//   i++;
+// }
+
+// For loop
+for (int i = 0; i < 3; i++)
+{
+ printf("meow\n");
+}
+}
