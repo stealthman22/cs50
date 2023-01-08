bc63442e028ee6e00f136018c4a9fbc3dea5323a
stealthman22 <tdotonline@gmail.com>
2023-01-08 18:32:36 +0000

    still having difficulties getting things to work locally, might just have to use codespaces

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
+// This is a header file, it imports the C standard library
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
- printf("hello, world\n");
+ // declaring data type string
+ string answer = get_string("What's your name? ");
+ printf("Hello, %s\n", answer);
 }
+
