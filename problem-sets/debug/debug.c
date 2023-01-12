02c1c8eda25049598a7439ed2b8eb475149762fd
stealthman22 <tdotonline@gmail.com>
2023-01-12 00:27:17 +0000

    sending updates

--- problem-sets/debug/debug.c
+++ problem-sets/debug/debug.c
@@ -1,14 +1,31 @@
+// // Become familiar wih C syntax
+// // Learn to debug buggy code
+
+// #include <cs50.h>
+
+// int main(void)
+// {
+//     // Ask for your name and where live
+//     name = get_string("What is your name? ")
+//     location = get_string("Where do you live? ")
+
+//     // Say hello
+//     print("Hello, %i, from %i!", name, location)
+// }
+
+// ######### ANSWER
+
 // Become familiar wih C syntax
 // Learn to debug buggy code
-
+#include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
     // Ask for your name and where live
-    name = get_string("What is your name? ")
-    location = get_string("Where do you live? ")
+  string name = get_string("What is your name?\n ");
+    string location = get_string("Where do you live?\n ");
 
     // Say hello
-    print("Hello, %i, from %i!", name, location)
+    printf("Hello, %s, from %s!", name, location);
 }
