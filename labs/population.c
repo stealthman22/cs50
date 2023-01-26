04476fa3a541ef872d14f437fb8d4bc1ba1ec742
stealthman22 <tdotonline@gmail.com>
2023-01-26 21:03:16 +0000

    done with week 1 labs yaaay

--- labs/population.c
+++ labs/population.c
@@ -4,10 +4,34 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int population_start;
+    int population_end;
+    int count = 0;
+    do
+    {
+      population_start =  get_int("Start Size: ");
+    }
+    while(population_start < 9);
 
     // TODO: Prompt for end size
 
-    // TODO: Calculate number of years until we reach threshold
+    do
+    {
+      population_end =  get_int("End Size: ");
+    }
+    while(population_end < population_start );
 
-    // TODO: Print number of years
+    // TODO: Calculate number of years until we reach threshold
+    for (int i = 0; i < population_end; i++ )
+    {
+      // the calculation
+    if (population_start != population_end || population_start > population_end)
+    {
+       population_start =  population_start  +  (population_start / 3) -   (population_start / 4);
+        // update the count variable
+       count += 1;
+    }
+    }
+     // TODO: Print number of years
+      printf("Years: %i", count);
 }
