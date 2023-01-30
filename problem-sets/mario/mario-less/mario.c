2fe4da5c59e16e10de4400880ca054197bc681da
stealthman22 <tdotonline@gmail.com>
2023-01-30 05:16:10 +0000

    got the first problem set, i had major help from stackoverflow, now i have to understand the solution

--- problem-sets/mario/mario-less/mario.c
+++ problem-sets/mario/mario-less/mario.c
@@ -1,34 +1,51 @@
-
 #include <cs50.h>
 #include <stdio.h>
 
-int print_pyramid(int floors);
+int get_floors(void);
+void print_floors(int pyramid);
 
 int main(void)
 {
-    int pyramid;
+int n = get_floors();
 
-    do
-     {
-      pyramid = get_int("How many floors should your pyramid have: ");
+print_floors(n);
+}
+
+
+// Getting user input
+
+int get_floors(void)
+{
+int floors;
+
+do
+{
+  floors = get_int("How many floors should your pyramid have: ");
+}
+while(floors < 1 );
+
+ return floors;
+}
+
+// Handling user input
+void print_floors(int pyramid)
+{
+  int column;
+  for (int i = 1; i <= pyramid; i++ )
+  {
+
+// Got to understand how this loop works
+    for (column = 0; column < i; column++)
+    {
+      printf("#");
     }
-    while( pyramid < 1);
+    printf("\n");
+  }
 
-    print_pyramid(pyramid);
 }
 
-int print_pyramid(floors)
-{
- string h = "#";
-for (int i = 0; i < floors; i++)
-{
- for (int j = 0; j < floors; j++)
- {
-  printf("#");
- }
- printf("\n");
-}
-return floors;
-}
+// Got the logic of this loop from stackoverflow.
+//  Seems we can loop over a variable created outside of a loop. That's wild tho.
 
-// 35
+//  Here we create a first loop, that will loop over what ever the user inputs as his choice of floors, (and also that loop will print a new line to the terminal)
+// Also, for every
